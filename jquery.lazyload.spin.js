1eeed6866b624a93058f14bbdb1c4c63ff5922d3
MarQuis Knox <MarQuisKnox@users.noreply.github.com>
2017-07-26 14:36:13 +0200

    Update jquery.lazyload.spin.js

--- jquery.lazyload.spin.js
+++ jquery.lazyload.spin.js
@@ -1,17 +1,11 @@
-/*
+/**
  * Lazy Load - jQuery plugin for lazy loading images
- *
  * Copyright (c) 2007-2013 Mika Tuupola
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
  *
- * Licensed under the MIT license:
- *   http://www.opensource.org/licenses/mit-license.php
- *
- * Project home:
- *   http://www.appelsiini.net/projects/lazyload
- *
+ * Project home: http://www.appelsiini.net/projects/lazyload
  * Version:  1.9.3
- *
- */
+*/
 
 // START:	spin.js
 /**
